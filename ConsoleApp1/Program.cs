90849fdac78173a823a750feb3d95fc079d18103
Danylo Zahorodniuk <s27167@pjwstk.edu.pl>
2024-03-12 13:29:06 +0100

    add GetAvg method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, User!!");
+int[] arr = {1,2,3,4,5};
+Console.WriteLine(GetAvg(arr));
+
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+
+    return (double)sum / arr.Length;
+}
