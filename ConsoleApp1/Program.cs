50449dd6078a0e81bec6dcd154efac34096e6314
danilzagor <danilzagor@gmail.com>
2024-03-18 21:25:54 +0100

    change variable name

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,11 @@ Console.WriteLine("Function get average");
 Console.WriteLine(GetAvg(arr));
 static double GetAvg(int[] arr)
 {
-    int sum = 0;
+    int sum1 = 0;
     foreach (var num in arr)
     {
-        sum += num;
+        sum1 += num;
     }
 
-    return (double)sum / arr.Length;
+    return (double)sum1 / arr.Length;
 }
