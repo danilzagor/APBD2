b25bd6f6986b1bb5d824172fae8d327aada11dff
danilzagor <danilzagor@gmail.com>
2024-03-18 21:13:46 +0100

    asdasd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, User!!");
-int[] arr = {1,2,3,4,5};
+int[] arr = {3,2,5,6,8,12,123};
+Console.WriteLine("Function get average");
 Console.WriteLine(GetAvg(arr));
-
+asfdsdf
 static double GetAvg(int[] arr)
 {
     int sum = 0;
